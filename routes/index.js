0ef5460a6942c4d3723cd02728d9c76fdc897d9e
tratnayake <t.ratnayake1@gmail.com>
2016-02-20 15:49:40 -0800

    Testing route addition

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// Use this route for when someone tries to sign-up synchronously.
+router.get('/signup-sync', function(req,res){
+	//Render to the user the signup-sync.ejs view found inside /views
+	res.render('signup-sync');
+})
+
 module.exports = router;
