fd288192a9c49cbf24e1f77dcc7b8c8b00bfdaf8
tratnayake <t.ratnayake1@gmail.com>
2016-02-20 16:47:37 -0800

    Added the database.json file and the jsondb requirement

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+//The file where users will be stored.
+var JsonDB = require('node-json-db');
+var db = new JsonDB("database",true,false);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +15,24 @@ router.get('/', function(req, res, next) {
 router.get('/signup-sync', function(req,res){
 	//Render to the user the signup-sync.ejs view found inside /views
 	res.render('signup-sync');
+})
+
+//Use this route for when someone posts the sign-up-sync form.
+router.post('/submit-registration-sync',function(req,res){
+	console.log("Someone submitted a registration form synchronously. \n" +
+		"It contained: \n");
+	//Output the contents of the request's body.
+	console.log(req.body);
+	//Lets create a user out of what we've got.
+	var user = new Object();
+	user.name = req.body.InputName;
+	user.password = req.body.InputPassword;
+	user.email = req.body.InputEmail;
+
+	//Lets also store it in a file (usually it would be stored in a database);
+	db.push("/users/",user);
+	console.log(db.getData("/"))
+
 })
 
 module.exports = router;
